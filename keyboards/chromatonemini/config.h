fa1108eecdeec1208f5b4b31254858f4349d95c5
3araht <3araht@gmail.com>
2021-04-17 14:47:46 +0900

    chromatonemini first commit.

---
+++ keyboards/chromatonemini/config.h
@@ -0,0 +1,336 @@
+/*
+Copyright 2021 3araht
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0xF4B4
+#define DEVICE_VER   0x0001
+#define MANUFACTURER 3araht
+#define PRODUCT      chromatonemini
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 7
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { E6, D7, C6, D4, D0, D1 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/*
+ * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
+ */
+// #define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+
+//#define LED_NUM_LOCK_PIN B0
+//#define LED_CAPS_LOCK_PIN B1
+//#define LED_SCROLL_LOCK_PIN B2
+//#define LED_COMPOSE_PIN B3
+//#define LED_KANA_PIN B4
+
+//#define BACKLIGHT_PIN B7
+//#define BACKLIGHT_LEVELS 3
+//#define BACKLIGHT_BREATHING
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN D3
+//#ifdef RGB_DI_PIN
+//#    define RGBLED_NUM 16
+//#    define RGBLIGHT_HUE_STEP 8
+//#    define RGBLIGHT_SAT_STEP 8
+//#    define RGBLIGHT_VAL_STEP 8
+//#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+//#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/*== all animations enable ==*/
+//#    define RGBLIGHT_ANIMATIONS
+/*== or choose animations ==*/
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_CHRISTMAS
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_RGB_TEST
+//#    define RGBLIGHT_EFFECT_ALTERNATING
+/*== customize breathing effect ==*/
+/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
+//#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
+/*==== use exp() and sin() ====*/
+//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
+//#endif
+
+
+#ifdef RGB_MATRIX_ENABLE
+
+/* ws2812 RGB MATRIX */
+#    define DRIVER_LED_TOTAL 116
+#    define RGB_MATRIX_KEYPRESSES  // reacts to keypresses
+
+// 2020/07/26 Maximum brighness is set to 120, by refering to
+// the max current value measured with USB monitor.
+// limits maximum brightness of LEDs to 120 out of 255.
+// If not defined maximum  brightness is set to 255
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
+
+//  https://docs.qmk.fm/#/feature_rgb_matrix
+//  Enable suspend mode.
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true
+
+#    ifdef AUDIO_ENABLE
+#        define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#        define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#        define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#        define DISABLE_RGB_MATRIX_BREATHING
+#        define DISABLE_RGB_MATRIX_BAND_SAT
+// #define DISABLE_RGB_MATRIX_BAND_VAL
+#        define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#        define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#        define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#        define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #define DISABLE_RGB_MATRIX_CYCLE_ALL
+#        define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#        define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#        define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#        define DISABLE_RGB_MATRIX_DUAL_BEACON
+#        define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#        define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+#        define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#        define DISABLE_RGB_MATRIX_RAINDROPS
+#        define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#        define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+#        define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#        define DISABLE_RGB_MATRIX_SPLASH
+#        define DISABLE_RGB_MATRIX_MULTISPLASH
+#        define DISABLE_RGB_MATRIX_SOLID_SPLASH
+#        define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#    else
+// #define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#        define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#        define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#        define DISABLE_RGB_MATRIX_BREATHING
+#        define DISABLE_RGB_MATRIX_BAND_SAT
+// #define DISABLE_RGB_MATRIX_BAND_VAL
+#        define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#        define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#        define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#        define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #define DISABLE_RGB_MATRIX_CYCLE_ALL
+#        define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#        define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#        define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#        define DISABLE_RGB_MATRIX_DUAL_BEACON
+#        define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#        define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+#        define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#        define DISABLE_RGB_MATRIX_RAINDROPS
+#        define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#        define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+#        define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#        define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#        define DISABLE_RGB_MATRIX_SPLASH
+// #define DISABLE_RGB_MATRIX_MULTISPLASH
+#        define DISABLE_RGB_MATRIX_SOLID_SPLASH
+// #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#    endif  // AUDIO_ENABLE
+
+// #define DISABLE_RGB_MATRIX_ALPHAS_MODS
+// #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+// #define DISABLE_RGB_MATRIX_BREATHING
+// #define DISABLE_RGB_MATRIX_BAND_SAT  // white background ver. of _BAND_VAL
+// #define DISABLE_RGB_MATRIX_BAND_VAL
+// #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+// #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+// #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+// #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #define DISABLE_RGB_MATRIX_CYCLE_ALL
+// #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+// #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+// #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+// #define DISABLE_RGB_MATRIX_DUAL_BEACON
+// #define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+// #define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+// #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #define DISABLE_RGB_MATRIX_RAINDROPS
+// #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS  //White ver of above.
+// #define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+// #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #define DISABLE_RGB_MATRIX_SPLASH
+// #define DISABLE_RGB_MATRIX_MULTISPLASH
+// #define DISABLE_RGB_MATRIX_SOLID_SPLASH
+// #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif  // RGB_MATRIX_ENABLE
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+// /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+// #define LOCKING_SUPPORT_ENABLE
+// /* Locking resynchronize hack */
+// #define LOCKING_RESYNC_ENABLE
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+//#define GRAVE_ESC_CTRL_OVERRIDE
+
+/*
+ * Force NKRO
+ *
+ * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
+ * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
+ * makefile for this to work.)
+ *
+ * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
+ * until the next keyboard reset.
+ *
+ * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
+ * fully operational during normal computer usage.
+ *
+ * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
+ * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
+ * bootmagic, NKRO mode will always be enabled until it is toggled again during a
+ * power-up.
+ *
+ */
+//#define FORCE_NKRO
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+/* Bootmagic Lite key configuration */
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
+
+/* Audio */
+#ifdef AUDIO_ENABLE
+#    define B6_AUDIO  // use PB6 = PIN10 as Audio output
+// #define DAC_SAMPLE_MAX 32768U
+#    define DAC_SAMPLE_MAX 65535U
+// #define AUDIO_CLICKY
+#    define NO_MUSIC_MODE
+// #define STARTUP_SONG SONG(FANTASIE_IMPROMPTU)
+// #define STARTUP_SONG SONG(NOCTURNE_OP_9_NO_1)
+// #define STARTUP_SONG SONG(USSR_ANTHEM)
+// #define STARTUP_SONG SONG(CAMPANELLA)
+#endif  //  AUDIO_ENABLE
+
+/*
+ * MIDI options
+ */
+
+/* Prevent use of disabled MIDI features in the keymap */
+//#define MIDI_ENABLE_STRICT 1
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+//#define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+#ifdef MIDI_ENABLE
+#    define MIDI_ADVANCED
+#    define MIDI_TONE_KEYCODE_OCTAVES 6
+#endif  //  MIDI_ENABLE
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 1
+
+/*
+ * Encoder options
+ */
+#ifdef ENCODER_ENABLE
+#    define ENCODERS_PAD_A { B5 }
+#    define ENCODERS_PAD_B { B4 }
+#    define ENCODER_RESOLUTION 4
+#    define TAP_CODE_DELAY 10
+#endif  // ENCODER_ENABLE
---
+++ keyboards/chromatonemini/keymaps/led/keymap.c
@@ -0,0 +1,319 @@
+/* Copyright 2021 3araht
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _SHIFT,
+    _FLIPBASE,
+    _FLIPSHIFT,
+    _FN
+};
+
+#ifdef RGB_MATRIX_ENABLE
+
+#define BASE_LAYER_COLOR  RGB_GOLDENROD
+#define SHIFT_LAYER_COLOR RGB_ORANGE
+#define FLIPB_LAYER_COLOR RGB_BLUE
+#define FLIPS_LAYER_COLOR RGB_PURPLE
+
+//  Don't change the DEFAULT_SCALE_COL value below. It must be 0.
+#define DEFAULT_SCALE_COL 0
+static uint8_t scale_indicator_col = DEFAULT_SCALE_COL;
+
+// Defines names for use in _FN layer to specify which column to be used to turn on the LEDs.
+// use this with led_fn_indicator[37][3], ex. led_fn_indicator[_FN_C2][0].
+enum fn_key_names {
+    _FN_K01= 0,
+    _FN_K02,
+    _FN_K03,
+    _FN_K04,
+    _FN_K05,
+    _FN_K06,
+    _FN_K07,
+    _FN_K08,
+    _FN_K09,
+    _FN_K10,
+    _FN_K11,
+    _FN_K12,
+    _FN_K13,
+    _FN_K14,
+    _FN_K15,
+    _FN_K16,
+    _FN_K17,
+    _FN_K18,
+    _FN_K19,
+    _FN_K20,
+    _FN_K21,
+    _FN_K22,
+    _FN_K23,
+    _FN_K24,
+    _FN_K25,
+    _FN_K26,
+    _FN_K27,
+    _FN_K28,
+    _FN_K29,
+    _FN_K30,
+    _FN_K31,
+    _FN_K32,
+    _FN_K33,
+    _FN_K34,
+    _FN_K35,
+    _FN_K36,
+    _FN_K37,
+};
+#endif  //  RGB_MATRIX_ENABLE
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    SHIFTDN = SAFE_RANGE,
+    SHIFTUP,
+
+    L_BASE,
+    L_SHIFT,
+    L_FLIPB,
+    L_FLIPS
+};
+
+// Long press: go to _FN layer, tap: MUTE
+#define FN_MUTE LT(_FN, KC_MUTE)
+
+// Used to set octave to MI_OCT_0
+extern midi_config_t midi_config;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT( \
+            FN_MUTE,                                                                                                                                                                             \
+            MI_BENDU,                                                                                                                                                                            \
+        MI_TRNSD, MI_TRNSU,  MI_C_1, MI_D_1, MI_E_1,  MI_Fs_1, MI_Ab_1, MI_Bb_1, MI_C_2, MI_D_2, MI_E_2, MI_Fs_2, MI_Ab_2, MI_Bb_2, MI_C_3,  MI_D_3,  MI_E_3, MI_Fs_3, MI_Ab_3, MI_Bb_3, MI_C_4, \
+            MI_BENDD,           MI_Db_1, MI_Eb_1, MI_F_1,  MI_G_1,  MI_A_1,  MI_B_1, MI_Db_2, MI_Eb_2, MI_F_2,  MI_G_2, MI_A_2,  MI_B_2, MI_Db_3, MI_Eb_3, MI_F_3,  MI_G_3,  MI_A_3,  MI_B_3     \
+    ),
+    /* Shift   This layer must locate 1 layer above _BASE layer. */
+    [_SHIFT] = LAYOUT( \
+            _______,                                                                                                                                                                                     \
+            _______,                                                                                                                                                                                     \
+        SHIFTDN, SHIFTUP,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+            _______,               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______      \
+    ),
+
+    /* Flip Base */
+    [_FLIPBASE] = LAYOUT( \
+            FN_MUTE,                                                                                                                                                                             \
+            MI_BENDU,                                                                                                                                                                            \
+        MI_TRNSU, MI_TRNSD,  MI_C_4, MI_Bb_3, MI_Ab_3,  MI_Fs_3, MI_E_3, MI_D_3, MI_C_3, MI_Bb_2, MI_Ab_2, MI_Fs_2, MI_E_2, MI_D_2, MI_C_2,  MI_Bb_1,  MI_Ab_1, MI_Fs_1, MI_E_1, MI_D_1, MI_C_1, \
+            MI_BENDD,           MI_B_3, MI_A_3, MI_G_3,  MI_F_3,  MI_Eb_3,  MI_Db_3, MI_B_2, MI_A_2, MI_G_2,  MI_F_2, MI_Eb_2,  MI_Db_2, MI_B_1, MI_A_1, MI_G_1,  MI_F_1,  MI_Eb_1,  MI_Db_1     \
+    ),
+    /* Flip Shift   This layer must locate 1 layer above _FLIPBASE layer. */
+    [_FLIPSHIFT] = LAYOUT( \
+            _______,                                                                                                                                                                                     \
+            _______,                                                                                                                                                                                     \
+        SHIFTUP, SHIFTDN,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+            _______,               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______      \
+    ),
+
+    [_FN] =  LAYOUT( \
+            _______,                                                                                                                                                                                     \
+            MI_VELU,                                                                                                                                                                                     \
+        MI_OCTD, MI_OCTU,     L_BASE,  L_FLIPB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+            MI_VELD,               L_SHIFT, L_FLIPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX      \
+    )
+};
+
+void keyboard_post_init_user(void) {
+    //  Set otave to MI_OCT_0
+    midi_config.octave = MI_OCT_0 - MIDI_OCTAVE_MIN;
+
+    default_layer_set(1UL << _BASE);
+};
+
+void shift_led_indicator_left(void){
+    if (scale_indicator_col > 0) {
+        scale_indicator_col--;
+    } else {
+        scale_indicator_col = 11;
+    }
+}
+void shift_led_indicator_right(void){
+    if (scale_indicator_col < 11) {
+        scale_indicator_col++;
+    } else {
+        scale_indicator_col = 0;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint8_t layer = biton32(layer_state);
+
+    switch (keycode) {
+
+        case L_BASE:
+            //  reset transpose value and scale_indicator_col to default.
+            midi_config.transpose = 0;
+            scale_indicator_col = DEFAULT_SCALE_COL;
+            default_layer_set(1UL << _BASE);
+            break;
+
+        case L_SHIFT:
+            //  reset transpose value and scale_indicator_col to default.
+            midi_config.transpose = 0;
+            scale_indicator_col = DEFAULT_SCALE_COL;
+            default_layer_set(1UL << _SHIFT);
+            break;
+
+        case L_FLIPB:
+            //  reset transpose value and scale_indicator_col to default.
+            midi_config.transpose = 0;
+            scale_indicator_col = DEFAULT_SCALE_COL;
+            default_layer_set(1UL << _FLIPBASE);
+            break;
+
+        case L_FLIPS:
+            //  reset transpose value and scale_indicator_col to default.
+            midi_config.transpose = 0;
+            scale_indicator_col = DEFAULT_SCALE_COL;
+            default_layer_set(1UL << _FLIPSHIFT);
+            break;
+
+        //  SHIFTDN and SHIFTUP can be pressed only when layer is _SHIFT or _FLIPSHIFT.
+        case SHIFTDN:
+            if (record->event.pressed) {
+                switch (layer) {
+                    case _SHIFT:
+                        shift_led_indicator_left();
+                        break;
+
+                    case _FLIPSHIFT:
+                        shift_led_indicator_right();
+                        break;
+                }
+            }
+            break;
+        case SHIFTUP:
+            if (record->event.pressed) {
+                switch (layer) {
+                    case _SHIFT:
+                        shift_led_indicator_right();
+                        break;
+
+                    case _FLIPSHIFT:
+                        shift_led_indicator_left();
+                        break;
+                }
+            }
+            break;
+    }
+    return true;
+}
+
+void set_led_scale_indicator(uint8_t r, uint8_t g, uint8_t b) {
+    uint8_t max_scale_indicator_led_loop;
+    uint8_t i;
+    max_scale_indicator_led_loop = scale_indicator_col = DEFAULT_SCALE_COL ? 12 : 9;
+    for (i = 0; i < max_scale_indicator_led_loop; i++) {
+        rgb_matrix_set_color(led_scale_indicator[scale_indicator_col][i], r, g, b);
+    }
+}
+
+#ifdef RGB_MATRIX_ENABLE
+void rgb_matrix_indicators_user(void) {
+    // uint32_t mode = rgblight_get_mode();
+
+    if (rgb_matrix_is_enabled()) {  // turn the lights on when it is enabled.
+
+        uint8_t layer = biton32(layer_state);
+        // uint8_t max_scale_indicator_led_loop;
+        uint8_t i;
+
+        switch (layer) {
+            case _BASE:
+                //  midi_config.transpose range: -6..+6.
+                //  scale_indicator_col   range: 0..11
+                //  at the base layer, scale indicator has to move the opposite way.
+                if ( midi_config.transpose <= 0 ) {  // 0 is included.
+                  scale_indicator_col = -1 * midi_config.transpose;
+                } else {  // 0 can't be included here since max scale_indicator_col is 11.
+                  scale_indicator_col = 12 - midi_config.transpose;
+                }
+
+                // max_scale_indicator_led_loop = scale_indicator_col = DEFAULT_SCALE_COL ? 12 : 9;
+                // for (i = 0; i < max_scale_indicator_led_loop; i++) {
+                //     rgb_matrix_set_color(led_scale_indicator[scale_indicator_col][i], BASE_LAYER_COLOR);
+                // }
+                set_led_scale_indicator(BASE_LAYER_COLOR);
+                break;
+
+            case _FLIPBASE:
+                //  midi_config.transpose range: -6..+6.
+                //  scale_indicator_col   range: 0..11
+                //  at the flipped base layer, scale indicator has to move the same way.
+                if ( midi_config.transpose < 0 ) {  // 0 is excluded.
+                  scale_indicator_col = 12 + midi_config.transpose;
+                } else {  // 0 is included.
+                  scale_indicator_col = midi_config.transpose;
+                }
+
+                // max_scale_indicator_led_loop = scale_indicator_col = DEFAULT_SCALE_COL ? 12 : 9;
+                // for (i = 0; i < max_scale_indicator_led_loop; i++) {
+                //     rgb_matrix_set_color(led_scale_indicator[scale_indicator_col][i], FLIPB_LAYER_COLOR);
+                // }
+                set_led_scale_indicator(FLIPB_LAYER_COLOR);
+                break;
+
+            case _SHIFT:
+                // max_scale_indicator_led_loop = scale_indicator_col = DEFAULT_SCALE_COL ? 12 : 9;
+                // for (i = 0; i < max_scale_indicator_led_loop; i++) {
+                //     rgb_matrix_set_color(led_scale_indicator[scale_indicator_col][i], SHIFT_LAYER_COLOR);
+                // }
+                set_led_scale_indicator(SHIFT_LAYER_COLOR);
+                break;
+
+            case _FLIPSHIFT:
+                // max_scale_indicator_led_loop = scale_indicator_col = DEFAULT_SCALE_COL ? 12 : 9;
+                // for (i = 0; i < max_scale_indicator_led_loop; i++) {
+                //     rgb_matrix_set_color(led_scale_indicator[scale_indicator_col][i], FLIPS_LAYER_COLOR);
+                // }
+                set_led_scale_indicator(FLIPS_LAYER_COLOR);
+                break;
+
+            case _FN:
+                for (i = 1; i < 5; i++) {
+                    rgb_matrix_set_color(i, RGB_SPRINGGREEN);  //  up(1) down(4) left(3) right(2)  keys
+                }
+                for (i = 0; i < 3; i++) {
+                    rgb_matrix_set_color(led_fn_indicator[_FN_K01][i], BASE_LAYER_COLOR);    //  L_BASE
+                    rgb_matrix_set_color(led_fn_indicator[_FN_K02][i], SHIFT_LAYER_COLOR);   //  L_SHIFT
+                    rgb_matrix_set_color(led_fn_indicator[_FN_K03][i], FLIPB_LAYER_COLOR);   //  L_FLIPB
+                    rgb_matrix_set_color(led_fn_indicator[_FN_K04][i], FLIPS_LAYER_COLOR);   //  L_FLIPS
+                }
+                break;
+        }
+    }
+}
+#endif
+
+#ifdef ENCODER_ENABLE
+void encoder_update_user(int8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+}
+#endif  // ENCODER_ENABLE
