5a5b9fc34d43fac68245aa99fa5dce3d30d0a5db
3araht <3araht@gmail.com>
2021-05-12 22:43:53 +0900

    json file updated.

--- keyboards/ct_macropad/keymaps/via_macropad/config.h
+++ keyboards/ct_macropad/keymaps/via_macropad/config.h
@@ -2,11 +2,11 @@
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
-#define CT_MACROPAD_MACROPAD
-#ifdef CT_MACROPAD_MACROPAD
-// Change the ID for VIA support.
-#    undef PRODUCT_ID
-#    define PRODUCT_ID   0xF4B3
-#    undef PRODUCT
-#    define PRODUCT ct_macropad12
-#endif
+// #define CT_MACROPAD_MACROPAD
+// #ifdef CT_MACROPAD_MACROPAD
+// // Change the ID for VIA support.
+// #    undef PRODUCT_ID
+// #    define PRODUCT_ID   0xF4B3
+// #    undef PRODUCT
+// #    define PRODUCT ct_macropad12
+// #endif
